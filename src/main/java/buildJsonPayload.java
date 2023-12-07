e9f5d0848239c35535becdb2fc97aa974254b9d0
zhang <zhang_zlf@outlook.com>
2023-12-08 00:01:54 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/main/java/ChatInterface.java
    #	src/main/java/chatApiHttpClient.java
    #	src/main/java/settingWindow.java

--- src/main/java/buildJsonPayload.java
+++ src/main/java/buildJsonPayload.java
@@ -1,7 +1,11 @@
 public class buildJsonPayload {
-    private StringBuilder payloadHead = new StringBuilder("{\"model\": \"gpt-3.5-turbo\",\"messages\": [");
-    private String historyMessage = "{\"role\": \"user\", \"content\":\""+ ChatInterface.input +"\"},";
-    private String historyReply = "{\"role\": \"assistant\", \"content\":\"" + chatApiHttpClient.outputMessage + "\"},";
+    public static StringBuilder payloadHead = new StringBuilder("{\"model\": \"gpt-3.5-turbo\",\"messages\": [");
+    public static String historyMessage = "{\"role\": \"user\", \"content\":\""+ ChatInterface.input +"\"},";
+    public static String historyReply = "{\"role\": \"assistant\", \"content\":\"" + chatApiHttpClient.outputMessage + "\"},";
 
+    //public static String historyAll = "";
 
+    public buildJsonPayload() {
+        payloadHead.append(historyMessage).append(historyReply);
+    }
 }
--- src/main/java/chatApiHttpClient.java
+++ src/main/java/chatApiHttpClient.java
@@ -8,7 +8,6 @@ import org.apache.http.impl.client.HttpClients;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Objects;
 
 public class chatApiHttpClient {
 
