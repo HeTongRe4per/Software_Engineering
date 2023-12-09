f238fd4470d1680403a0157f48873403c568641c
zhang <zhang_zlf@outlook.com>
2023-12-09 18:01:13 +0800

    修改设置部分组件，主要是增加

--- src/main/java/settingWindow.java
+++ src/main/java/settingWindow.java
@@ -38,6 +38,10 @@ public class settingWindow extends JFrame{
         label4 = new JLabel();
         spinner1 = new JSpinner();
         button1 = new JButton();
+        label5 = new JLabel();
+        comboBox1 = new JComboBox();
+        button4 = new JButton();
+        label6 = new JLabel();
         button2 = new JButton();
         button3 = new JButton();
 
@@ -61,9 +65,9 @@ public class settingWindow extends JFrame{
             layeredPane1.add(label2, JLayeredPane.DEFAULT_LAYER);
             label2.setBounds(new Rectangle(new Point(35, 70), label2.getPreferredSize()));
             layeredPane1.add(textField2, JLayeredPane.DEFAULT_LAYER);
-            textField2.setBounds(100, 20, 190, 25);
+            textField2.setBounds(110, 20, 200, 25);
             layeredPane1.add(textField1, JLayeredPane.DEFAULT_LAYER);
-            textField1.setBounds(100, 65, 191, 25);
+            textField1.setBounds(110, 60, 200, 25);
         }
 
         //======== layeredPane2 ========
@@ -71,21 +75,38 @@ public class settingWindow extends JFrame{
             layeredPane2.setBorder(new TitledBorder("Chat\u6846\u8bbe\u7f6e"));
 
             //---- label3 ----
-            label3.setText("Chat\u6846\u5b57\u4f53");
+            label3.setText("\u5b57\u4f53");
             layeredPane2.add(label3, JLayeredPane.DEFAULT_LAYER);
-            label3.setBounds(new Rectangle(new Point(15, 30), label3.getPreferredSize()));
+            label3.setBounds(45, 30, 30, label3.getPreferredSize().height);
 
             //---- label4 ----
-            label4.setText("Chat\u6846\u989c\u8272");
+            label4.setText("\u5b57\u4f53\u989c\u8272");
             layeredPane2.add(label4, JLayeredPane.DEFAULT_LAYER);
-            label4.setBounds(new Rectangle(new Point(155, 30), label4.getPreferredSize()));
+            label4.setBounds(190, 70, 50, label4.getPreferredSize().height);
             layeredPane2.add(spinner1, JLayeredPane.DEFAULT_LAYER);
-            spinner1.setBounds(85, 25, 55, 25);
+            spinner1.setBounds(245, 25, 70, 25);
 
             //---- button1 ----
-            button1.setText("1");
+            button1.setText("\u4fee\u6539");
             layeredPane2.add(button1, JLayeredPane.DEFAULT_LAYER);
-            button1.setBounds(230, 25, 55, 25);
+            button1.setBounds(245, 65, 70, 25);
+
+            //---- label5 ----
+            label5.setText("\u80cc\u666f\u989c\u8272");
+            layeredPane2.add(label5, JLayeredPane.DEFAULT_LAYER);
+            label5.setBounds(new Rectangle(new Point(30, 70), label5.getPreferredSize()));
+            layeredPane2.add(comboBox1, JLayeredPane.DEFAULT_LAYER);
+            comboBox1.setBounds(90, 25, 70, 25);
+
+            //---- button4 ----
+            button4.setText("\u4fee\u6539");
+            layeredPane2.add(button4, JLayeredPane.DEFAULT_LAYER);
+            button4.setBounds(90, 65, 70, 25);
+
+            //---- label6 ----
+            label6.setText("\u5b57\u53f7");
+            layeredPane2.add(label6, JLayeredPane.DEFAULT_LAYER);
+            label6.setBounds(new Rectangle(new Point(210, 30), label6.getPreferredSize()));
         }
 
         //---- button2 ----
@@ -102,13 +123,13 @@ public class settingWindow extends JFrame{
                 .addGroup(contentPaneLayout.createSequentialGroup()
                     .addContainerGap()
                     .addGroup(contentPaneLayout.createParallelGroup()
+                        .addComponent(layeredPane1, GroupLayout.DEFAULT_SIZE, 353, Short.MAX_VALUE)
                         .addGroup(GroupLayout.Alignment.TRAILING, contentPaneLayout.createSequentialGroup()
-                            .addGap(0, 203, Short.MAX_VALUE)
+                            .addGap(0, 223, Short.MAX_VALUE)
                             .addComponent(button3, GroupLayout.PREFERRED_SIZE, 60, GroupLayout.PREFERRED_SIZE)
                             .addPreferredGap(LayoutStyle.ComponentPlacement.UNRELATED)
                             .addComponent(button2, GroupLayout.PREFERRED_SIZE, 60, GroupLayout.PREFERRED_SIZE))
-                        .addComponent(layeredPane2)
-                        .addComponent(layeredPane1, GroupLayout.DEFAULT_SIZE, 333, Short.MAX_VALUE))
+                        .addComponent(layeredPane2))
                     .addContainerGap())
         );
         contentPaneLayout.setVerticalGroup(
@@ -117,14 +138,14 @@ public class settingWindow extends JFrame{
                     .addGap(17, 17, 17)
                     .addComponent(layeredPane1, GroupLayout.PREFERRED_SIZE, 106, GroupLayout.PREFERRED_SIZE)
                     .addPreferredGap(LayoutStyle.ComponentPlacement.UNRELATED)
-                    .addComponent(layeredPane2, GroupLayout.PREFERRED_SIZE, 70, GroupLayout.PREFERRED_SIZE)
+                    .addComponent(layeredPane2, GroupLayout.DEFAULT_SIZE, 120, Short.MAX_VALUE)
                     .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
                     .addGroup(contentPaneLayout.createParallelGroup()
                         .addComponent(button2, GroupLayout.PREFERRED_SIZE, 25, GroupLayout.PREFERRED_SIZE)
                         .addComponent(button3, GroupLayout.PREFERRED_SIZE, 25, GroupLayout.PREFERRED_SIZE))
-                    .addContainerGap(15, Short.MAX_VALUE))
+                    .addContainerGap())
         );
-        setSize(355, 280);
+        setSize(375, 325);
         setLocationRelativeTo(null);
         // JFormDesigner - End of component initialization  //GEN-END:initComponents  @formatter:on
     }
@@ -140,6 +161,10 @@ public class settingWindow extends JFrame{
     private JLabel label4;
     private JSpinner spinner1;
     private JButton button1;
+    private JLabel label5;
+    private JComboBox comboBox1;
+    private JButton button4;
+    private JLabel label6;
     private JButton button2;
     private JButton button3;
     // JFormDesigner - End of variables declaration  //GEN-END:variables  @formatter:on
