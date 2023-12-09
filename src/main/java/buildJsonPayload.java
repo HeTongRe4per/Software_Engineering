61817110e0f393da8dc2558b722e2c15ee34be09
zhang <zhang_zlf@outlook.com>
2023-12-09 09:27:35 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/main/java/ChatInterface.java
    #	src/main/java/chatApiHttpClient.java
    #	src/main/java/settingWindow.java

--- src/main/java/buildJsonPayload.java
+++ src/main/java/buildJsonPayload.java
@@ -1,5 +1,15 @@
 public class buildJsonPayload {
     public static StringBuilder payload = new StringBuilder("{\"model\": \"gpt-3.5-turbo\",\"messages\": [");
-    public static String historyMessage = "{\"role\": \"user\", \"content\":\""+ ChatInterface.input +"\"},";
-    public static String historyReply = "{\"role\": \"assistant\", \"content\":\"" + chatApiHttpClient.outputMessage + "\"},";
+    public static String historyMessage;
+    public static String historyReply;
+
+    public static void getOutputMessage() {
+        String output = chatApiHttpClient.outputMessage;
+        historyReply = "{\"role\": \"assistant\", \"content\":\"" + output + "\"},";
+    }
+
+    public static void refreshInputMessage() {
+        String input = ChatInterface.inputMessage;
+        historyMessage = "{\"role\": \"user\", \"content\":\"" + ChatInterface.inputMessage + "\"},";
+    }
 }
