02e7f23aacc6a292bb0718ba2034a705a1ad4e72
zhang <zhang_zlf@outlook.com>
2023-12-09 19:01:14 +0800

    将ChatAPI输出改为了UTF-8

--- src/main/java/chatApiHttpClient.java
+++ src/main/java/chatApiHttpClient.java
@@ -8,6 +8,7 @@ import org.apache.http.impl.client.HttpClients;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 
 public class chatApiHttpClient {
 
@@ -44,7 +45,7 @@ public class chatApiHttpClient {
             HttpResponse response = httpClient.execute(httpPost);
 
             // 读取响应内容
-            try (BufferedReader reader = new BufferedReader(new InputStreamReader(response.getEntity().getContent()))) {
+            try (BufferedReader reader = new BufferedReader(new InputStreamReader(response.getEntity().getContent(),StandardCharsets.UTF_8))) {
                 StringBuilder output = new StringBuilder();
                 String line;
                 while ((line = reader.readLine()) != null) {
