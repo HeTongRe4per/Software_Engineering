a9bdf934fd7a483f7b76463e470ab120bb193805
小样 <2506350436@qq.com>
2023-12-22 15:49:56 +0800

    数据库部分基本完成，可以让狗叫男测试了。

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -9,17 +9,6 @@ public class main {
         SwingUtilities.invokeLater(new Runnable() {
             @Override
             public void run() {
-                // 创建 MySQL 数据库连接
-                try {
-                    Connection connection = createMySQLConnection();
-                    // 在这里可以使用数据库连接执行其他操作
-
-                    // 关闭数据库连接
-                    connection.close();
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                    // 处理数据库连接异常
-                }
                 try {
                     // 设置本机系统外观
                     UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
@@ -29,21 +18,5 @@ public class main {
                 new loginWindows().setVisible(true);
             }
         });
-    }
-//
-    public static Connection createMySQLConnection() throws SQLException {
-        String url = "jdbc:mysql://database.hetong-re4per.icu/chatgpt_account";
-        String username = "chatgpt";
-        String password = "zl221021@Chatgpt";
-
-        // 加载 MySQL 驱动程序
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-        } catch (ClassNotFoundException e) {
-            throw new SQLException("MySQL JDBC Driver not found", e);
-        }
-
-        // 创建数据库连接
-        return DriverManager.getConnection(url, username, password);
     }
 }
